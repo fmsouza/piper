64ce9d0cba58e22598e301d5fa2d2931ebb19f35
Fred Souza <fred@MacBook.local>
2016-08-25 22:06:44 -0300

    Creating pipelining api for GET requests

---
+++ index.js
@@ -0,0 +1,35 @@
+/* jshint esversion: 6 */
+const url = require("url");
+
+const router = {};
+
+function doRouting(url, req, res, body) {
+    const apis = url.pathname.split('/');
+    for (let api of apis) {
+        const middleware = router[`/${api}`];
+        if (middleware) body = middleware(req, res, body);
+    }
+    return body;
+}
+
+function prepareBody(req, url) {
+    let body = '';
+    switch (req.method) {
+        case 'GET': default:
+            body = url.query || {};
+    }
+    return body;
+}
+
+const Segserver = function(req, res) {
+    const parsedUrl = url.parse(req.url, true);
+    let body = prepareBody(req, parsedUrl);
+    body = doRouting(parsedUrl, req, res, body);
+    res.end(JSON.stringify(body));
+};
+
+Segserver.route = function(route, fn) {
+    router[route] = fn;
+};
+
+module.exports = Segserver;
---
+++ test/test.js
@@ -0,0 +1,81 @@
+/* jshint esversion: 6 */
+const http = require('http');
+const expect = require('expect');
+const querystring = require('querystring');
+
+const segserver = require('../index');
+const request = require('../src/request');
+
+const hostname = 'localhost';
+const port = 3000;
+
+function increment(req, res, body) {
+  body.value = parseInt(body.value) + 1;
+  return body;
+}
+
+function multiply(req, res, body) {
+  body.value = parseInt(body.a) * parseInt(body.b);
+  delete body.a;
+  delete body.b;
+  return body;
+}
+
+describe('test/', () => {
+
+  before(() => {
+    segserver.route('/increment', increment);
+    segserver.route('/multiply', multiply);
+
+    server = http.createServer(segserver);
+    server.listen(port, hostname);
+  });
+
+  it('should do nothing in base path and return "bar"', (done) => {
+    const query = querystring.stringify({ 'foo': 'bar' });
+    request.get(`http://${hostname}:${port}/?${query}`)
+    .then((res) => {
+      var body = JSON.parse(res.body);
+      expect(res.statusCode).toBe(200);
+      expect(body.foo).toBe('bar');
+      done();
+    });
+  });
+
+  it('should increment 2 by 1 and become 3', (done) => {
+    const query = querystring.stringify({ 'value': 2 });
+    request.get(`http://${hostname}:${port}/increment/?${query}`)
+    .then((res) => {
+      var body = JSON.parse(res.body);
+      expect(res.statusCode).toBe(200);
+      expect(body.value).toBe(3);
+      done();
+    });
+  });
+
+  it('should increment 2 by 2 and become 4 by pipelining "increment" twice', (done) => {
+    const query = querystring.stringify({ 'value': 2 });
+    request.get(`http://${hostname}:${port}/increment/increment/?${query}`)
+    .then((res) => {
+      var body = JSON.parse(res.body);
+      expect(res.statusCode).toBe(200);
+      expect(body.value).toBe(4);
+      done();
+    });
+  });
+
+  it('should multiply 5 with 7 and increment by 1 pipelining "increment" with "multiply"', (done) => {
+    const query = querystring.stringify({ a: 5, b: 7 });
+    request.get(`http://${hostname}:${port}/multiply/increment/?${query}`)
+    .then((res) => {
+      var body = JSON.parse(res.body);
+      expect(res.statusCode).toBe(200);
+      expect(body.value).toBe(36);
+      done();
+    });
+  });
+
+  after(() => {
+    server.close();
+  });
+});
